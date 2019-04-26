18b170c95396111e2b20818497823f74e096ac78
eches <Elaine.Chesoni83@gmail.com>
2019-04-25 23:22:26 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask, flash, g, jsonify, redirect, render_template, request, url_for
+from flask_wtf import FlaskForm
+from wtforms import Form, TextField
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = config['DEFAULT']['SECRET_KEY']
+
+# app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", default=None)
+# if not SECRET_KEY:
+#     raise ValueError("No secret key set for Flask application")
+
+class TextMessageForm(FlaskForm):
+    text_message = TextField('text_message')
+
+
+@app.route("/", methods=['GET','POST'])
+def home():
+    form = TextMessageForm()
+
+    if form.validate_on_submit():
+        if request.method == 'POST':
+            text_message = form.text_message.data
+            response_message = text_message[2::3]
+            flash(response_message)
+
+            return redirect(url_for('home'))
+
+    return render_template("home.html", form=form)
+
+@app.route("/test", methods=['POST'])
+def test():
+    data = request.get_json()
+    string_to_cut = data['string_to_cut']
+    return_string = data['string_to_cut'][2::3]
+
+    return jsonify({'return_string' : return_string})
+
+if __name__ == "__main__":
+    app.run(debug=True)
